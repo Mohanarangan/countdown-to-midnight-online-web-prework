ff2474d6e9d9b49971a9f61b22f0f67b6df3403d
chandra mohan <mohaniyer_78@hotmail.com>
2019-02-26 09:01:44 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,6 +3,18 @@ number = 10
 
 def countdown(number)
   while number> -1
+  sleep 1
+  puts "#{number} SECOND(S)"
+  number-=1
+  year = "HAPPY NEW YEAR!"
+end
+  
+end
+
+
+def countdown(number)
+  while number> -1
+  sleep 1
   puts "#{number} SECOND(S)"
   number-=1
   year = "HAPPY NEW YEAR!"
